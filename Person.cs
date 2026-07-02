bb74f4ced8e72d1c355b300e41433c9dacd59c86
mert714 <mertmyumyun09@gmail.com>
2026-07-02 10:50:52 +0300

    Create Person Class

---
+++ Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+
+        public Person(string name, int age, double salary)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"Име: {Name},Възраст: {Age}, Заплата: {Salary: F2}";
+        }
+    }
+}

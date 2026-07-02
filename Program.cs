3660597cc8ac4ef8fbb702273e40542d56f360ec
mert714 <mertmyumyun09@gmail.com>
2026-07-02 11:55:00 +0300

    Create Menu

--- Program.cs
+++ Program.cs
@@ -10,7 +10,21 @@ namespace FirstProject
     {
         static void Main(string[] args)
         {
+            List<Person> people = LoadPeopleFromFile();
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine("--Избери операция--");
+                Console.WriteLine("1.CREATE(Добавяне на човек)");
+                Console.WriteLine("2.READ(Показване на всички)");
+                Console.WriteLine("3.UPDATE(Промяна на заплата)");
+                Console.WriteLine("4.DELETE(Изтриване по име)");
+                Console.WriteLine("5.Изход");
+                Console.Write("Избор:");
 
+                string choice = Console.ReadLine();
+                Console.WriteLine();
+            }
         }
     }
 }
